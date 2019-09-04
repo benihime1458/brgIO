84bdef7a6ad8c1f130c20cd14987df2dd5731eab
baklavacado <benny.l.wu@gmail.com>
2019-09-03 23:04:08 -0700

    add css and app description

--- frontEnd/client/src/Components/Layout/Dashboard.jsx
+++ frontEnd/client/src/Components/Layout/Dashboard.jsx
@@ -19,11 +19,16 @@ const useStyles = makeStyles((theme => ({
     height: '100%',
   },
   content: {
+    display: 'flex',
+    justifyContent: 'center',
     height: '100%',
-    padding: theme.spacing(15),
+    padding: theme.spacing(4),
+    paddingTop: theme.spacing(14),
   },
   paper: {
     height: '100%',
+    minWidth: '50%',
+    padding: theme.spacing(5),
   },
 })));
 
@@ -31,14 +36,17 @@ export default props => {
   const classes = useStyles();
   const [index, setIndex] = useState(0);
 
-  props.user ? console.log('logged in as', props.user.email) : console.log('not logged in');
-
+  // props.user ? console.log('logged in as', props.user.email) : console.log('not logged in');
+  // props.userList ? console.log('userList', props.userList) : null;
+  // const users = props.userList;
+  // console.log('userList', users);
   return (
     <div className={classes.root}>
       <div className={classes.content}>
         <Paper className={classes.paper}>
-          {!props.user ?
+          {!props.user ? 
             <>
+              <Typography variant="h1" align="center" gutterBottom>brg.io</Typography>
               <Tabs
                 value={index}
                 indicatorColor='primary'
@@ -49,7 +57,10 @@ export default props => {
                 <Tab label='Login' onClick={() => setIndex(0)} />
                 <Tab label='Sign Up' onClick={() => setIndex(1)} />
               </Tabs>
-              {index == 0 ? <Login /> : <Signup />}
+              {index == 0 ? <Login /> : <Signup userList={props.userList}/>}
+              <Typography paragraph >
+                Welcome to brg.io! My name is Benny and I love bouldering. This web app is for quickly logging your bouldering session at Brides Rock Gym, my home gym. More features will be added as I continue to work on this project. =]
+              </Typography>
             </>
             :
             <>
