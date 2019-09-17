ca31cb43ab3f278e97d47d118291c9e31fa82301
baklavacado <benny.l.wu@gmail.com>
2019-09-16 18:53:54 -0700

    Render problems by area in separate tables

--- frontEnd/client/src/Components/Climbs/climbs-list.jsx
+++ frontEnd/client/src/Components/Climbs/climbs-list.jsx
@@ -2,13 +2,26 @@ import React, { Fragment, Component, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-import { withStyles } from '@material-ui/core/styles';
-import {
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { Card, CardMedia,
   Table, TableBody, TableCell, TableHead,
   TableRow, Paper, Typography,
 } from '@material-ui/core';
 
 const hosts = 'http://ec2-54-183-225-234.us-west-1.compute.amazonaws.com:5635' || 'http://localhost:5635';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    marginBottom: 25,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  typography: {
+    align: 'center',
+  },
+}));
 
 const StyledTableCell = withStyles(theme => ({
   head: {
@@ -28,21 +41,18 @@ const StyledTableRow = withStyles(theme => ({
   },
 }))(TableRow);
 
-const Climb = props => (
+const Climb = props => ( 
   <StyledTableRow>
     <StyledTableCell>{props.climb.number}</StyledTableCell>
     <StyledTableCell>{props.climb.color}</StyledTableCell>
-    <StyledTableCell>{props.climb.grade}</StyledTableCell>
+    <StyledTableCell>V{props.climb.grade}</StyledTableCell>
     <StyledTableCell>{props.climb.dateSet.substring(0, 10)}</StyledTableCell>
-    <StyledTableCell>{props.climb.area}</StyledTableCell>
-    {/* <StyledTableCell>
-      <Link to={"/edit/" + props.climb._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.climb._id) }}>delete</a>
-    </StyledTableCell> */}
   </StyledTableRow>
 )
 
 export default props => {
   const [climbs, setClimbs] = useState([]);
+  const classes = useStyles();
 
   useEffect(() => {
     axios.get(`http://localhost:5635/climbs`)
@@ -52,19 +62,16 @@ export default props => {
       .catch((error) => console.log(error))
   }, []);
 
-  const climbList = () => {
-    return climbs.map(climb => {
+  const climbList = (wall) => {
+    let problems = climbs.filter(climb => climb.area === wall);
+    problems.sort((a, b) => a.number - b.number);
+    problems = problems.map(climb => {
       return <Climb climb={climb} key={climb._id}/>
     })
 
-  }
-  
-  const list = climbList();
-  
-  return (
-    <Fragment>
-      <Typography variant='h4' gutterBottom>Bouldering Log</Typography>
-      <Paper>
+    return <Fragment>
+      <Typography align="center" variant='h4' gutterBottom>{wall.toUpperCase()}</Typography>
+      <Card className={classes.card}>
         <Table>
           <TableHead>
             <TableRow>
@@ -72,14 +79,39 @@ export default props => {
               <StyledTableCell>Color</StyledTableCell>
               <StyledTableCell>Grade</StyledTableCell>
               <StyledTableCell>Date Set</StyledTableCell>
-              <StyledTableCell>Area</StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {list}
+            {problems}
           </TableBody>
         </Table>
-      </Paper>
+      </Card>
+    </Fragment> 
+  }
+  
+  const cave = climbList('cave');
+  const corridor = climbList('corridor');
+  const northeast = climbList('north east');
+  const slab = climbList('slab');
+  const southeast = climbList('south east');
+  const southwest = climbList('south west');
+  const toprope = climbList('top rope');
+  const westwall = climbList('west wall');
+  
+  return (
+    <Fragment>
+      <Typography align="center" variant='h4' gutterBottom>ROUTESETTING MAP</Typography>
+        <Card className={classes.card}>
+          <CardMedia className={classes.media} image="images/brgmap.jpg" title="brgmap"/>
+        </Card>
+        {cave}
+        {corridor}
+        {northeast}
+        {slab}
+        {southeast}
+        {southwest}
+        {westwall}
+        {toprope}
     </Fragment>
   )
 }
