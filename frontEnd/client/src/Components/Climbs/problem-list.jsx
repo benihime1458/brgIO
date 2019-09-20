700b9626a557d5c032d9984c8e25459b3a647d24
baklavacado <benny.l.wu@gmail.com>
2019-09-20 13:47:05 -0700

    consolidate columns , add attempts/sends counters

--- frontEnd/client/src/Components/Climbs/problem-list.jsx
+++ frontEnd/client/src/Components/Climbs/problem-list.jsx
@@ -2,10 +2,12 @@ import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom'; // use to edit climbing notes and other details
 
 import { makeStyles, withStyles } from '@material-ui/core/styles';
-import { Card, CardMedia, Checkbox,
+import { Card, CardContent, CardMedia, Checkbox,
   Table, TableBody, TableCell, TableHead,
-  TableRow, Typography, Button
+  TableRow, Typography, Divider, IconButton,
 } from '@material-ui/core';
+import Add  from '@material-ui/icons/Add';
+import Remove  from '@material-ui/icons/Remove';
 
 const hosts = 'http://ec2-54-183-225-234.us-west-1.compute.amazonaws.com:5635' || 'http://localhost:5635';
 
@@ -44,20 +46,40 @@ const Problem = props => {
   let currentProblem = props.problem;
   let [flashed, setFlashed] = useState(currentProblem.flashed)
   let [project, setProject] = useState(currentProblem.flashed)
-  const classes = props.classes;
+  let [attempts, setAttempts] = useState(currentProblem.attempts)
+  let [sends, setSends] = useState(currentProblem.sends)
 
   return (<StyledTableRow>
-      <StyledTableCell align="center">{currentProblem.number}</StyledTableCell>
-      <StyledTableCell align="center">V{currentProblem.grade}</StyledTableCell>
-      <StyledTableCell align="center">{<Button fullWidth variant="outlined" style={{backgroundColor: `${currentProblem.color}`, color: 'white'}}>{currentProblem.color}</Button>}</StyledTableCell>
-      {/* <StyledTableCell>{currentProblem.dateSet.substring(0, 10)}</StyledTableCell> */}
-      <StyledTableCell align="center">{currentProblem.attempts}</StyledTableCell>
-      <StyledTableCell align="center">{currentProblem.sends}</StyledTableCell>
-      <StyledTableCell align="center">
-      {<Checkbox color="primary" checked={flashed} onClick={() => flashed ? setFlashed(false) : setFlashed(true)}/>}
+      <StyledTableCell align="center" width="150px">
+        <Card style={{ width: '100%'}} >
+          <CardContent style={{ backgroundColor: '#eeeeee'}}>
+            <Card>
+              <Typography variant="body2" style={{backgroundColor: 'white'}}>{currentProblem.number}</Typography>
+              <Divider/>
+              <Typography variant="subtitle2" style={{backgroundColor: 'white'}}>V{currentProblem.grade}</Typography>
+              <Divider/>
+              <Typography style={{ backgroundColor: `${currentProblem.color}`, height: '20px'}} ></Typography>
+            </Card>
+          </CardContent>
+        </Card>
       </StyledTableCell>
       <StyledTableCell align="center">
-      {<Checkbox checked={project} onClick={() => project ? setProject(false) : setProject(true)}/>}
+      <Typography variant="subtitle2">
+        <IconButton onClick={() => setAttempts(attempts - 1)} color="secondary"><Remove fontSize="small" /></IconButton>
+        {attempts}
+        <IconButton onClick={() => setAttempts(attempts + 1)} color="primary"><Add fontSize="small" /></IconButton></Typography>
+      </StyledTableCell>
+      <StyledTableCell align="center">
+      <Typography variant="subtitle2">
+        <IconButton onClick={() => setSends(sends - 1)} color="secondary"><Remove fontSize="small" /></IconButton>
+        {sends}
+        <IconButton onClick={() => setSends(sends + 1)} color="primary"><Add fontSize="small" /></IconButton></Typography>
+      </StyledTableCell>
+      <StyledTableCell align="center">
+      {<Checkbox checked={flashed} onClick={() => flashed ? setFlashed(false) : setFlashed(true)}/>}
+      </StyledTableCell>
+      <StyledTableCell align="center">
+      {<Checkbox color="primary" checked={project} onClick={() => project ? setProject(false) : setProject(true)}/>}
       </StyledTableCell>
     </StyledTableRow>)
 }
@@ -79,10 +101,7 @@ export default props => {
         <Table>
           <TableHead>
             <TableRow>
-              <StyledTableCell align="center">Number</StyledTableCell>
-              <StyledTableCell align="center">Grade</StyledTableCell>
-              <StyledTableCell align="center">Color</StyledTableCell>
-              {/* <StyledTableCell>Date Set</StyledTableCell> */}
+              <StyledTableCell align="center"># | Grade | Color</StyledTableCell>
               <StyledTableCell align="center">Attempts</StyledTableCell>
               <StyledTableCell align="center">Sends</StyledTableCell>
               <StyledTableCell align="center">Flash</StyledTableCell>
