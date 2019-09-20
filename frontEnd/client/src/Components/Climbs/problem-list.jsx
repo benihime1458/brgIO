da04874d2e4b7645b2c4388e6f4e85944d5417c4
baklavacado <benny.l.wu@gmail.com>
2019-09-19 22:37:22 -0700

    add flashed, project states / render colors

--- frontEnd/client/src/Components/Climbs/problem-list.jsx
+++ frontEnd/client/src/Components/Climbs/problem-list.jsx
@@ -1,10 +1,10 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom'; // use to edit climbing notes and other details
 
 import { makeStyles, withStyles } from '@material-ui/core/styles';
-import { Card, CardMedia,
+import { Card, CardMedia, Checkbox,
   Table, TableBody, TableCell, TableHead,
-  TableRow, Typography,
+  TableRow, Typography, Button
 } from '@material-ui/core';
 
 const hosts = 'http://ec2-54-183-225-234.us-west-1.compute.amazonaws.com:5635' || 'http://localhost:5635';
@@ -40,18 +40,27 @@ const StyledTableRow = withStyles(theme => ({
   },
 }))(TableRow);
 
-const Problem = props => (
-  <StyledTableRow>
-    <StyledTableCell>{props.problem.number}</StyledTableCell>
-    <StyledTableCell>{props.problem.color}</StyledTableCell>
-    <StyledTableCell>V{props.problem.grade}</StyledTableCell>
-    <StyledTableCell>{props.problem.dateSet.substring(0, 10)}</StyledTableCell>
-    <StyledTableCell>{props.problem.attempts}</StyledTableCell>
-    <StyledTableCell>{props.problem.sends}</StyledTableCell>
-    <StyledTableCell>{props.problem.flashed}</StyledTableCell>
-    <StyledTableCell>{props.problem.project}</StyledTableCell>
-  </StyledTableRow>
-)
+const Problem = props => {
+  let currentProblem = props.problem;
+  let [flashed, setFlashed] = useState(currentProblem.flashed)
+  let [project, setProject] = useState(currentProblem.flashed)
+  const classes = props.classes;
+
+  return (<StyledTableRow>
+      <StyledTableCell align="center">{currentProblem.number}</StyledTableCell>
+      <StyledTableCell align="center">V{currentProblem.grade}</StyledTableCell>
+      <StyledTableCell align="center">{<Button fullWidth variant="outlined" style={{backgroundColor: `${currentProblem.color}`, color: 'white'}}>{currentProblem.color}</Button>}</StyledTableCell>
+      {/* <StyledTableCell>{currentProblem.dateSet.substring(0, 10)}</StyledTableCell> */}
+      <StyledTableCell align="center">{currentProblem.attempts}</StyledTableCell>
+      <StyledTableCell align="center">{currentProblem.sends}</StyledTableCell>
+      <StyledTableCell align="center">
+      {<Checkbox color="primary" checked={flashed} onClick={() => flashed ? setFlashed(false) : setFlashed(true)}/>}
+      </StyledTableCell>
+      <StyledTableCell align="center">
+      {<Checkbox checked={project} onClick={() => project ? setProject(false) : setProject(true)}/>}
+      </StyledTableCell>
+    </StyledTableRow>)
+}
 
 export default props => {
 
@@ -61,7 +70,7 @@ export default props => {
     let problems = props.user.problemLog.filter(problem => problem.area === wall);
     problems.sort((a, b) => a.number - b.number);
     problems = problems.map(problem => {
-      return <Problem problem={problem} key={problem._id}/>
+      return <Problem problem={problem} key={problem._id} classes={classes}/>
     })
 
     return <Fragment>
@@ -70,14 +79,14 @@ export default props => {
         <Table>
           <TableHead>
             <TableRow>
-              <StyledTableCell>Number</StyledTableCell>
-              <StyledTableCell>Color</StyledTableCell>
-              <StyledTableCell>Grade</StyledTableCell>
-              <StyledTableCell>Date Set</StyledTableCell>
-              <StyledTableCell>Attempts</StyledTableCell>
-              <StyledTableCell>Sends</StyledTableCell>
-              <StyledTableCell>Flashed</StyledTableCell>
-              <StyledTableCell>Project</StyledTableCell>
+              <StyledTableCell align="center">Number</StyledTableCell>
+              <StyledTableCell align="center">Grade</StyledTableCell>
+              <StyledTableCell align="center">Color</StyledTableCell>
+              {/* <StyledTableCell>Date Set</StyledTableCell> */}
+              <StyledTableCell align="center">Attempts</StyledTableCell>
+              <StyledTableCell align="center">Sends</StyledTableCell>
+              <StyledTableCell align="center">Flash</StyledTableCell>
+              <StyledTableCell align="center">Project</StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
