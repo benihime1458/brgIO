6e0c547d248f83f28b43eed7a933967722e91d69
baklavacado <benny.l.wu@gmail.com>
2019-10-02 12:21:25 -0700

    update problem log by area in user object

--- backEnd/database/routes/users.js
+++ backEnd/database/routes/users.js
@@ -1,9 +1,45 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
+let Problem = require('../models/problem.model');
 
 router.route('/').get((req, res) => {
   User.find()
     .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:username/').get((req, res) => {
+  User.findOne({username: req.params.username})
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:username/problemLog').get((req, res) => {
+  User.findOne({ username: req.params.username })
+    .then(user => res.json(user.problemLog))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:username/problemLog/:area').get((req, res) => {
+  User.findOne({username: req.params.username})
+  .then(user => res.json(user.problemLog.filter(problem => problem.area === req.params.area)))
+  .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+router.route('/:username/problemLog/:area/update').post((req, res) => {
+  User.findOne({username: req.params.username})
+    .then(user => {
+      Problem.find({area: req.params.area})
+      .then(problem => {
+        user.problemLog = [...user.problemLog.filter(problems => problems.area != req.params.area), ...problem]
+        // res.json(user.problemLog)
+        user.save()
+          .then(() => res.json(`${req.params.area} updated`))
+          .catch(err => res.status(400).json('Error: ' + err));
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+    })
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
