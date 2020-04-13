389d1d49e805504e169a0e01608670945b4a32f1
baklavacado <benny.l.wu@gmail.com>
2020-04-13 01:24:12 -0700

    deploy on netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
 module.exports = {
     entry: [
         './frontEnd/client/src/index.js',
@@ -22,8 +28,20 @@ module.exports = {
     resolve: {
         extensions: ['*', '.js', '.jsx'],
     },
+    optimization: {
+        minimizer: [new UglifyJsPlugin()],
+    },
+    plugins: [
+        // New plugin
+        new HtmlWebpackPlugin({
+            // injects bundle.js to our new index.html
+            inject: true,
+            // copys the content of the existing index.html to the new /build index.html
+            template: path.resolve('./frontEnd/client/dist/index.html'),
+        }),
+    ],
     output: {
-        path: `${__dirname}/frontEnd/client/dist`,
+        path: `${__dirname}/frontEnd/client/build`,
         publicPath: '/',
         filename: 'bundle.js',
     },
